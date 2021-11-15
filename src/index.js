4397c0d466e82dab6413248ef0de58a956ea573d
Gabriel Rufino <contato@gabrielrufino.com>
2021-11-15 18:58:45 -0300

    First version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 'use strict'
 
 async function main () {
+  const [,, text] = process.argv
+  let input = ''
+  let charactersCount = 0
 
+  while (!input.includes(text)) {
+    const character = String.fromCharCode(
+      Math.floor(Math.random() * 128)
+    )
+    input += character
+
+    if (input.length > 10000) {
+      input = input.substring(1)
+    }
+
+    charactersCount++
+  }
+
+  console.log(text, input, charactersCount)
 }
 
 main()
