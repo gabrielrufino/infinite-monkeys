2ea2b32f8c2953269e963b8198c0d4aaf1bb2544
Gabriel Rufino <contato@gabrielrufino.com>
2021-11-15 23:24:27 -0300

    Using helper generateCharacter

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 
+import { generateCharacter } from './helpers/index.js'
+
 async function main () {
   const args = yargs(hideBin(process.argv))
     .command('type [text]', 'Make the monkey type madly')
@@ -19,9 +21,7 @@ async function main () {
   let charactersCount = 0
 
   while (!input.includes(args.text)) {
-    const character = String.fromCharCode(
-      Math.floor(Math.random() * 128)
-    )
+    const character = generateCharacter()
     input += character
 
     if (input.length > 10000) {
