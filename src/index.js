56286d60ff751cb9cf0703f71cebb74e284c4bc1
Gabriel Rufino <contato@gabrielrufino.com>
2021-11-17 21:37:34 -0300

    Using worker threads

--- src/index.js
+++ src/index.js
@@ -4,34 +4,58 @@
 
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
+import { Worker, isMainThread, parentPort, workerData } from 'worker_threads'
 
 import { generateCharacter } from './helpers/index.js'
 
 async function main () {
-  const args = yargs(hideBin(process.argv))
-    .command('type [text]', 'Make the monkey type madly')
-    .option('monkeys', {
-      alias: 'm',
-      type: 'number',
-      description: 'Number of monkeys'
+  if (isMainThread) {
+    const args = yargs(hideBin(process.argv))
+      .command('type [text]', 'Make the monkey type madly')
+      .option('monkeys', {
+        alias: 'm',
+        type: 'number',
+        description: 'Number of monkeys',
+        default: 1
+      })
+      .parse()
+
+    const monkeys = Array(args.monkeys)
+      .fill(undefined)
+      .map(() => new Worker(import.meta.url.replace('file://', ''), {
+        workerData: {
+          text: args.text
+        }
+      }))
+
+    monkeys.forEach(monkey => {
+      monkey.on('message', result => {
+        console.log(result)
+        process.exit(0)
+      })
     })
-    .parse()
+  } else {
+    const { text } = workerData
 
-  let input = ''
-  let charactersCount = 0
+    let input = ''
+    let charactersCount = 0
 
-  while (!input.includes(args.text)) {
-    const character = generateCharacter()
-    input += character
+    while (!input.includes(text)) {
+      const character = generateCharacter()
+      input += character
 
-    if (input.length > 10000) {
-      input = input.substring(1)
+      if (input.length > 10000) {
+        input = input.substring(1)
+      }
+
+      charactersCount++
     }
 
-    charactersCount++
+    parentPort.postMessage({
+      input,
+      charactersCount
+    })
   }
-
-  console.log(args.text, input, charactersCount)
 }
 
 main()
