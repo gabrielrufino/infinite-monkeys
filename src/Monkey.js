171f31dd64111e38a390cb3ab13ada37a6deb71d
Gabriel Rufino <contato@gabrielrufino.com>
2022-03-19 13:33:44 -0300

    Worker file

--- src/Monkey.js
+++ src/Monkey.js
@@ -6,6 +6,8 @@ export default class Monkey extends EventEmitter {
   constructor ({ id, text }) {
     super()
 
+    console.log('test')
+
     this.id = id
     this.text = text
     this.count = 0
--- src/index.js
+++ src/index.js
@@ -1,51 +1,39 @@
 #!/usr/bin/env node
 
 import yargs from 'yargs'
+import path from 'path'
 import { hideBin } from 'yargs/helpers'
-import { Worker, isMainThread, workerData } from 'worker_threads'
+import { Worker } from 'worker_threads'
 
-import Monkey from './Monkey.js'
-import MonkeyListeners from './MonkeyListeners.js'
 import WorkerListeners from './WorkerListeners.js'
 
 async function main () {
-  if (isMainThread) {
-    const args = yargs(hideBin(process.argv))
-      .command('type [text]', 'Make the monkey type madly')
-      .option('monkeys', {
-        alias: 'm',
-        type: 'number',
-        description: 'Number of monkeys',
-        default: 1
-      })
-      .parse()
-
-    Array(args.monkeys)
-      .fill(undefined)
-      .map((_, index) => new Worker(import.meta.url.replace('file://', ''), {
-        workerData: {
-          id: index + 1,
-          text: args.text
-        }
-      }))
-      .forEach(monkey => {
-        monkey.on('message', event => {
-          WorkerListeners[event.type](event)
-        })
-      })
-  } else {
-    const { id, text } = workerData
-
-    const monkey = new Monkey({
-      id,
-      text
+  const args = yargs(hideBin(process.argv))
+    .command('type [text]', 'Make the monkey type madly')
+    .option('monkeys', {
+      alias: 'm',
+      type: 'number',
+      description: 'Number of monkeys',
+      default: 1
     })
+    .parse()
 
-    monkey.on('update', MonkeyListeners.update)
-    monkey.on('match', MonkeyListeners.match)
+  const dirname = path.dirname(new URL(import.meta.url).pathname)
+  const worker = path.join(dirname, 'worker.js')
 
-    monkey.type()
-  }
+  Array(args.monkeys)
+    .fill(undefined)
+    .map((_, index) => new Worker(worker, {
+      workerData: {
+        id: index + 1,
+        text: args.text
+      }
+    }))
+    .forEach(monkey => {
+      monkey.on('message', event => {
+        WorkerListeners[event.type](event)
+      })
+    })
 }
 
 main()
