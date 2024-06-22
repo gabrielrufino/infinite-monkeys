721f7e279b9cd9d669ef6eb14cc4069f8e38b6be
Gabriel Rufino <contato@gabrielrufino.com>
2024-06-22 11:58:51 -0300

    fix: lint fix

--- src/MonkeyListeners.js
+++ src/MonkeyListeners.js
@@ -8,10 +8,4 @@ export default class MonkeyListeners {
       charactersCount: count
     })
   }
-
-  static update (_event) {
-    parentPort.postMessage({
-      type: MonkeyListeners.update.name
-    })
-  }
 }
